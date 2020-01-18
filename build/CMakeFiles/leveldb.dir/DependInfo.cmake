af408262de9d2dede74e6d908bf2f47025c28907
xp <xpsair@qq.com>
2020-01-18 15:37:05 +0800

    applied 1080Ti CUDA compile configs

---
+++ build/CMakeFiles/leveldb.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xp/flying/LevelDB-CUDA/db/builder.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/builder.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/c.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/c.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/db_impl.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/db_impl.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/db_iter.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/db_iter.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/dbformat.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/dbformat.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/dumpfile.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/dumpfile.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/filename.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/filename.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/log_reader.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/log_reader.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/log_writer.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/log_writer.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/memtable.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/memtable.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/repair.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/repair.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/table_cache.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/table_cache.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/version_edit.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/version_edit.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/version_set.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/version_set.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/db/write_batch.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/db/write_batch.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/helpers/memenv/memenv.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/helpers/memenv/memenv.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/table/block.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/table/block.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/table/block_builder.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/table/block_builder.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/table/filter_block.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/table/filter_block.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/table/format.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/table/format.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/table/iterator.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/table/iterator.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/table/merger.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/table/merger.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/table/table.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/table/table.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/table/table_builder.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/table/table_builder.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/table/two_level_iterator.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/table/two_level_iterator.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/arena.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/arena.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/bloom.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/bloom.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/cache.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/cache.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/coding.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/coding.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/comparator.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/comparator.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/crc32c.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/crc32c.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/env.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/env.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/env_posix.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/env_posix.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/filter_policy.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/filter_policy.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/hash.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/hash.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/logging.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/logging.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/options.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/options.cc.o"
+  "/home/xp/flying/LevelDB-CUDA/util/status.cc" "/home/xp/flying/LevelDB-CUDA/build/CMakeFiles/leveldb.dir/util/status.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LEVELDB_COMPILE_LIBRARY"
+  "LEVELDB_PLATFORM_POSIX=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "../"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/paper_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/paper_test.dir/util/testharness.cc.o"
+  "CMakeFiles/paper_test.dir/util/testutil.cc.o"
+  "CMakeFiles/paper_test.dir/db/paper_test.cc.o"
+  "paper_test.pdb"
+  "paper_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/paper_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
